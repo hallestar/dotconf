c90dbb4b330380beb62ccca0038e04659c018325
hallestar <wongzicai@gmail.com>
2020-03-12 12:40:24 +0800

    format

--- configure.sh
+++ configure.sh
@@ -1,12 +1,14 @@
-# tmux
+# ----------------------------------- tmux-----------------------------------
 if [ -f $HOME/.tmux.conf ] || [ -h $HOME/.tmux.conf ]; then
     mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 fi
 echo "ln -s $PWD/.tmux.conf $HOME/.tmux.conf ..."
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 echo "done..."
+# ----------------------------------- tmux-----------------------------------
 
-# zsh
+
+# ----------------------------------- zsh -----------------------------------
 if [ -f $HOME/.zshrc ] || [ -h $HOME/.zshrc ]; then
     mv $HOME/.zshrc $HOME/.zshrc.bak
 fi
@@ -18,8 +20,10 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 echo "done..."
+# ----------------------------------- zsh -----------------------------------
 
-# vim
+
+# ----------------------------------- vim -----------------------------------
 if [ -f $HOME/.vimrc ] || [ -h $HOME/.vimrc ]; then
     mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
@@ -29,4 +33,4 @@ if [ ! -d $HOME/.vim/bundle ]; then
     git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 echo "done..."
-
+# ----------------------------------- vim -----------------------------------
