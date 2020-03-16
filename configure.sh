1b45e1c60b1fa02aba0dacc53a64ff73a89fa407
huangzicai <huangzicai@g90-unknown307080.i.nease.net>
2020-03-16 11:47:14 +0800

    autosuggestions

--- configure.sh
+++ configure.sh
@@ -15,8 +15,12 @@ ln -s $PWD/.zshrc $HOME/.zshrc
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
+
 echo "done..."
 
 # vim
