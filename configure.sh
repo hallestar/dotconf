130b5c9f727b7046c8be7d8d3576a1d6cd7e4e20
hallestar <wongzicai@gmail.com>
2020-12-18 17:46:19 +0800

    add highlight syntax

--- configure.sh
+++ configure.sh
@@ -21,6 +21,10 @@ if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
+if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
 echo "done..."
 
 # vim
